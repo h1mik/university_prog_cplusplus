4a82e5a673fd51e2a82e9e40cba91af118915275
h1mik <seigneur.h1mik@gmail.com>
2020-10-27 12:37:57 +0200

    Add new file: lab_2.3.19(9)

---
+++ C++/chapter_2/lab_2.3.19(9).cpp
@@ -0,0 +1,23 @@
+﻿    //////////////////////////////////////////////
+    //                                          //
+    //               STUDENT                    //
+    //       Karpenko Vladyslav Igorovych       //
+    //       GROUP: ISD-21                      //
+    //       TELEGRAM:  @h1mik                  //
+    //       E-MAIL: seigneur.h1mik@gmail.com   //
+    //                                          //
+    //////////////////////////////////////////////
+    
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int number,sum;
+    cin>>number;
+    sum=(pow((number-1),2))+1;
+    cout<<sum;
+}
