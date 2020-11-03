3efc2fe3aad1b2f30378aa0a5c6636ca6f5751fc
h1mik <seigneur.h1mik@gmail.com>
2020-11-03 17:57:16 +0200

    Add nwe code for 2.4.2

--- C++/chapter_2/lab_2.4.2.cpp
+++ C++/chapter_2/lab_2.4.2.cpp
@@ -1,6 +1,6 @@
 ﻿#include<iostream>
 using namespace std;
-int dec2bin(int num)
+int bin(int num)
 {
     int bin = 0, k = 1;
 
@@ -15,12 +15,38 @@ int dec2bin(int num)
 }
 int main()
 {
+    int i=0;
     int number;
     cout << "\n";
     cin >>number;
-    cout<<dec2bin(number)<<"\n";
-    int a = dec2bin(number);
+    cout<<bin(number)<<"\n";
+    int a = bin(number);
     cout<<a;
+    for(;a>0;i++){
+            a/=10;
+        }
+    cout<<"\n"<<i<<"\n";
+    int A[15];
+    
+    for(int r=0; r<(16-i);r++){
+        A[r]=0;
+    }
+   a = bin(number);
+    int TEMP=a;
+    for(int r=15; r>0;r--){
+        A[r]=TEMP%10;
+        TEMP /= 10;
+    }
+    for(int re; re<15;re++){
+       cout<<A[re];
+    }
+    cout<<A[15];
+    if((A[0]==A[15])&&(A[1]==A[14])&&(A[2]==A[13])&&(A[3]==A[12])&&(A[4]==A[11])&&(A[5]==A[10])&&(A[6]==A[9])&&(A[7]==A[8])){
+        cout<<"\nyes";
+    }
+    else{
+        cout<<"\nno";
+    }
 
     return 0;
 }
