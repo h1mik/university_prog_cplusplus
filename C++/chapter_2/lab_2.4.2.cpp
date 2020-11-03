8311f092b8c458997b73ad6e439b8fb3d4097c9f
h1mik <seigneur.h1mik@gmail.com>
2020-11-03 11:43:53 +0200

    Create lab_2.4.2.cpp

---
+++ C++/chapter_2/lab_2.4.2.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+using namespace std;
+int dec2bin(int num)
+{
+    int bin = 0, k = 1;
+
+    while (num)
+    {
+        bin += (num % 2) * k;
+        k *= 10;
+        num /= 2;
+    }
+
+    return bin;
+}
+int main()
+{
+    int number;
+    cout << "\n";
+    cin >>number;
+    cout<<dec2bin(number)<<"\n";
+    int a = dec2bin(number);
+    cout<<a;
+
+    return 0;
+}
