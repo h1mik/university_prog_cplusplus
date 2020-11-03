50d344ad0ecdc85faebe403f587add8c5fae6f6a
h1mik <seigneur.h1mik@gmail.com>
2020-11-03 11:35:02 +0200

    Add new files: lab_24.1; lab

---
+++ C++/chapter_2/lab_2.4.1.cpp
@@ -0,0 +1,39 @@
+﻿    //////////////////////////////////////////////
+    //                                          //
+    //               STUDENT                    //
+    //       Karpenko Vladyslav Igorovych       //
+    //       GROUP: ISD-21                      //
+    //       TELEGRAM:  @h1mik                  //
+    //       E-MAIL: seigneur.h1mik@gmail.com   //
+    //                                          //
+    //////////////////////////////////////////////
+    
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int number;
+    label1:
+    cout<<"Введите число:\n";
+    cin>>number;
+    int sum=0;
+    
+    if(number>=0)
+    {
+        // 538
+        while(number)
+        {
+            sum=sum+(number&1);
+            number>>=1;
+        }
+        cout<<sum;
+    }
+    else
+    {
+        cout<<"Неправильное число, введите ещё раз";
+        goto label1;
+    }
+    
+}
